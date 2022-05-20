16ac3ff172d1d73307e6d6e07205b4820f520ea4
AlinaLevkova77 <91475522+AlinaLevkova77@users.noreply.github.com>
2022-05-20 12:43:01 +0200

    10

---
+++ src/index.js
@@ -0,0 +1,85 @@
+import './css/styles.css';
+import fetchCountries from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+
+
+const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo:document.querySelector('.country-info'),
+}
+refs.input.addEventListener('input', debounce(onInputSearch,DEBOUNCE_DELAY));
+
+function clearData() {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+}
+
+function onInputSearch(e) {
+    const inputValue = e.target.value.trim();
+
+    if (inputValue === "") {
+        clearData();
+        return;
+    }
+
+
+    fetchCountries('inputValue')
+        .then(countries => {
+            if (countries.length > 10) {
+                clearData();
+                Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+                return;
+            } else if (countries.length === 1) {
+                clearData();
+                renderCountry(countries[0]);
+                return;
+            }
+            renderCountries(countries);
+       
+        })
+        .catch(error => {
+            clearData();
+            Notiflix.Notify.failure('"Oops, there is no country with that name"');
+            console.log(error);
+            return;
+        });
+
+    function renderCountry(country) {
+        console.log(country);
+        const markup = `
+    <div class = 'country'>
+    <img src = '${country.flags.svg}' alt ='${country.name.official} class = 'flag'/>
+    <h1>${country.name.official}</h1>
+    <p><span>Capital:</span>${country.capital}</p>
+    <p><span>Population:</span>${country.population}</p>
+    <p><span>Languages:</span>${country.languages}.join(',')</p>`
+        refs.countryInfo.innerHTML = markup;
+    }
+
+    function renderCountries(countries) {
+        
+        const countriesMarcup = countries.map((country) => {
+            return `
+        <li>
+        <img src = '${country.flags.svg}' alt ='${country.name.official}'/>
+        <span>${country.name.official}</span>
+        </li>`
+        
+        }).join('');
+        refs.countryList.innerHTML = countriesMarcup;
+    }
+}
+    
+
+
+   
+    
+
+
+
+
+
